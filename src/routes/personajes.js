abeb19c3f7f42ee028853373d0ecbdc0a2b4d825
Dominique236 <dominique.soto@uc.cl>
2025-01-02 19:30:40 -0300

    feat: conexión a la api

--- src/routes/personajes.js
+++ src/routes/personajes.js
@@ -2,26 +2,70 @@ const Router = require('koa-router');
 
 const router = new Router();
 
-const characters = [
-    {
-        name: "Homer Simpson",
-        description: "Homer Jay Simpson is a fictional character and the main protagonist of the American animated sitcom The Simpsons. He is voiced by Dan Castellaneta and first appeared on television, along with the rest of his family, in The Tracey Ullman Show short 'Good Night' on April 19, 1987.",
-        image: "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png"
-    },
-    {
-        name: "Marge Simpson",
-        description: "Marjorie Jacqueline Simpson is a fictional character in the American animated sitcom The Simpsons and part of the eponymous family. She is voiced by Julie Kavner and first appeared on television in The Tracey Ullman Show short 'Good Night' on April 19, 1987.",
-        image: "https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png"
-    },
-    {
-        name: "Bart Simpson",
-        description: "Bartholomew JoJo Simpson is a fictional character in the American animated television series The Simpsons and part of the Simpson family. He is voiced by Nancy Cartwright and first appeared on television in The Tracey Ullman Show short 'Good Night' on April 19, 1987.",
-        image: "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png"
+// Entregar un personaje, según su parametro id entregado en la ruta
+// respuesta: el personaje (id exite) o error (id no existe)
+router.get("personajes.show", "/:id", async (ctx) => {
+    try{
+        const personaje = await ctx.orm.Personaje.findOne({where:{id:ctx.params.id}});
+        ctx.body = personaje;
+        ctx.status = 200;
+    } catch {
+        ctx.body = { message: "El personaje no existe" };
+        ctx.status = 404;
     }
-]
+});
 
-router.get("personajes.show", "/show", async (ctx) => {
-    ctx.body = characters;
-})
+// Entregar todos los personajes
+// respuesta: los personajes encontrados
+router.get("personajes.list", "/", async (ctx) => {
+    try{
+        const personajes = await ctx.orm.Personaje.findAll();
+        ctx.body = personajes;
+        ctx.status = 200;
+    } catch(error){
+        ctx.body = error;
+        ctx.status = 400;
+        console.log(error)
+    }
+});
+
+// Entregar todos los personajes no disponibles para el jugador con nombre :nombre (ya elegidos)
+// respuesta: los personajes no disponibles
+router.get("personajes.list", "/nodisponibles/:nombre", async (ctx) => {
+    try{
+        // Encuentra al usuario con ese nombre
+        const usuario = await ctx.orm.Usuario.findOne({
+            where: {
+            nombre: ctx.params.nombre
+            }
+        });
+        // Encuentra al jugador de ese usuario
+        const jugador = await ctx.orm.Jugador.findOne({
+            where: {
+            usuarioId: usuario.id
+            }
+        });
+        // Encuentra todos los jugadores de esa partida
+        const todosJugadores = await ctx.orm.Jugador.findAll({
+            where: {partidaId:jugador.partidaId}
+        });
+        // Filtra los jugadores cuyo arcanoId no sea null
+        const jugadoresConArcano = todosJugadores.filter(jugador => jugador.arcanoId !== null);
+        // Itera sobre los jugadores filtrados y encuentra los personajes correspondientes
+        const personajes = await Promise.all(jugadoresConArcano.map(async (jugador) => {
+            const personaje = await ctx.orm.Personaje.findOne({
+                where: { id: jugador.arcanoId }
+            });
+            return personaje;
+        }));
+        console.log('Arcanos no disponibles:',personajes);
+        ctx.body = personajes;
+        ctx.status = 200;
+    } catch(error){
+        ctx.body = error;
+        ctx.status = 400;
+        console.log(error)
+    }
+});
 
 module.exports = router;
